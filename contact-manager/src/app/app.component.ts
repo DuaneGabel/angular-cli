3fdbdf54620178a3530c7881ab3baf851c0adacd
dgabel001 <duane_gabel@cable.comcast.com>
2019-01-03 15:58:10 -0700

    02_06

--- contact-manager/src/app/app.component.ts
+++ contact-manager/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'contact-manager';
+
+  ngOnInit() {
+    particlesJS.load('particles', 'assets/particles.json', () => {
+      console.log('Particles loaded ...');
+    })
+  }
 }
