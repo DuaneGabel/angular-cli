c2904d0df8f3138e9e66e9396596c2b00a6943ea
dgabel001 <duane_gabel@cable.comcast.com>
2019-01-07 16:39:31 -0700

    03_03

--- contact-manager/src/app/app.component.ts
+++ contact-manager/src/app/app.component.ts
@@ -3,14 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'contact-manager';
-
-  // ngOnInit() {
-  //   particlesJS.load('particles', 'assets/particles.json', () => {
-  //     console.log('Particles loaded ...');
-  //   })
-  // }
+  title = 'cm';
 }
